4ed74912803de3ed41c12cb48781c55c5fa8fa46
azamat.bekmuratov <azamat.bekmuratov@gmail.com>
2017-10-16 22:35:33 +0600

    added short bubble sort

--- SearchAndSorting/buble/bublesort.py
+++ SearchAndSorting/buble/bublesort.py
@@ -9,6 +9,24 @@ def bubble_sort(alist):
                 alist[i + 1] = temp
 
 
+def short_bubble_sort(alist):
+    exchanges = True
+    passnum = len(alist) - 1
+    while passnum > 0 and exchanges:
+        exchanges = False
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                exchanges = True
+                temp = alist[i]
+                alist[i] = alist[i + 1]
+                alist[i + 1] = temp
+        passnum = passnum - 1
+
+
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
 bubble_sort(alist)
 print(alist)
+
+alist2=[20, 30, 40, 90, 50, 60, 70, 80, 100, 110]
+short_bubble_sort(alist2)
+print(alist2)
